9829f36688ebdf34cbf0af9673929197b17e922d
janvisahu31 <sahujanvi317@gmail.com>
2025-06-11 23:08:05 +0530

    subsets backtracking algorithms

---
+++ backtracking/subsets.py
@@ -0,0 +1,15 @@
+def subsets(nums):
+    res = []
+
+    def backtrack(start, path):
+        res.append(path[:])
+        for i in range(start, len(nums)):
+            path.append(nums[i])
+            backtrack(i + 1, path)
+            path.pop()
+
+    backtrack(0, [])
+    return res
+
+if __name__ == "__main__":
+    print(subsets([1,2,3]))
